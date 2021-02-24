576cd2a2e56901e9c7610d1f5518171f9a05194b
a4681636 <f49427362000@yahoo.com.tw>
2021-02-24 21:30:28 +0800

    時間

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 $("a").each(function (index, element) {
     
-    console.log("編號 + " + index)
+    // console.log("編號 + " + index)
 
     var target = $(this).attr("data-st-target");
     var duration = $(this).attr("data-st-duration");
 
     console.log("目標:" + target);
     console.log("時間:" + duration);
+
+    var offset = $("#" + target).offset();
+    var top = offset.top;
+    
+
+     console.log("上方:" + top);
+
+     $(this).click(function (e) { 
+         e.preventDefault();
+// parseInt 文字轉數字
+         $("html").stop().animate({
+             scrollTop:top
+         },parseInt(duration)) 
+         
+     });
+    
     
 });
